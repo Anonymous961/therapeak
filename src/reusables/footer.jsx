4f3cceb386d284e39cb4c3006314c5ec7cf9cb14
Anil Kumar Behera <96728156+Anonymous961@users.noreply.github.com>
2023-04-25 16:18:43 +0530

    still working

---
+++ src/reusables/footer.jsx
@@ -0,0 +1,17 @@
+import './styles/footer.css';
+
+const Foot = () => {
+    return ( 
+        <div className="container-fluid foot">
+            <div className="container">
+                <div className="row">
+                    <div className="col">
+                    <img src="https://i.postimg.cc/YSrf7DV9/therapeak-Logo.png" alt="" /> <h2 style={{float:"right",marginTop:"1.5rem"}}>| Therapeak</h2>
+                    </div>
+                </div>
+            </div>
+        </div>
+     );
+}
+ 
+export default Foot;
