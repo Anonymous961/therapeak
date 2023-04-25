2a1561be023b46863144c4c8d05ea87337bff4be
Anil Kumar Behera <96728156+Anonymous961@users.noreply.github.com>
2023-04-25 17:01:22 +0530

    footer complete

--- src/reusables/footer.jsx
+++ src/reusables/footer.jsx
@@ -1,4 +1,5 @@
 import './styles/footer.css';
+import {Link} from 'react-router-dom';
 
 const Foot = () => {
     return ( 
@@ -6,7 +7,61 @@ const Foot = () => {
             <div className="container">
                 <div className="row">
                     <div className="col">
-                    <img src="https://i.postimg.cc/YSrf7DV9/therapeak-Logo.png" alt="" /> <h2 style={{float:"right",marginTop:"1.5rem"}}>| Therapeak</h2>
+                        <a href="#upper-land-page" style={{textDecoration:"none"}}>
+                            <img src="https://i.postimg.cc/YSrf7DV9/therapeak-Logo.png" alt="" /> <h2 style={{marginTop:"1.5rem"}}>| Therapeak</h2>
+                        </a>
+                    </div>
+                    <div className="col">
+                        <h3>Our Sponsors</h3>
+                        <br />
+                        <p>lego</p>
+                        <p>McDonald’s</p>
+                        <p>Dell</p>
+                        <p>KaufLand</p>
+                    </div>
+                    <div className="col">
+                        <h3>Resources</h3>
+                        <br />
+                        <p>Resources</p>
+                        <p>Help & Support</p>
+                        <p>Reviews</p>
+                        <p>Community</p>
+                    </div>
+                    <div className="col">
+                        <h3>Company</h3>
+                        <br />
+                        <p>Contact Us</p>
+                        <p>About Us</p>
+                        <p>Trust, Safety & Security</p>
+                        <p>Our Impact</p>
+                    </div>
+                </div>
+                <hr />
+                <div className="row">
+                    <div className="col">
+                    <p>
+                    © 2023  Therapeak  Inc.
+                    </p>
+                    </div>
+                    <div className="col">
+                    <p>
+                    Terms of Service
+                    </p>
+                    </div>
+                    <div className="col">
+                    <p>
+                    Privacy Policy
+                    </p>
+                    </div>
+                    <div className="col">
+                    <p>
+                    Cookie Settings
+                    </p>
+                    </div>
+                    <div className="col">
+                    <p>
+                    Accessibility
+                    </p>
                     </div>
                 </div>
             </div>
