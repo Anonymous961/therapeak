d04ce48c4cf1ebce83a96ede715ccb1684022de0
LemonDrop847 <66952539+LemonDrop847@users.noreply.github.com>
2023-04-26 21:28:58 +0530

    Firebase changed

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -4,12 +4,12 @@ import { getAuth } from "firebase/auth";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA55DUP6zRYZdElb5SHb0lJZhXzv_W-QVw",
-  authDomain: "therapeak-9c8a4.firebaseapp.com",
-  projectId: "therapeak-9c8a4",
-  storageBucket: "therapeak-9c8a4.appspot.com",
-  messagingSenderId: "390472427452",
-  appId: "1:390472427452:web:781c0ea29d6d1ae591edb9"
+  apiKey: "AIzaSyACO5JYGjIcH70qPGjFit25kIIbe5Waw5Q",
+  authDomain: "therapeak-7fb59.firebaseapp.com",
+  projectId: "therapeak-7fb59",
+  storageBucket: "therapeak-7fb59.appspot.com",
+  messagingSenderId: "188853442891",
+  appId: "1:188853442891:web:e91d03959f0b536f7c30ce"
 };
 
 // init firebase
