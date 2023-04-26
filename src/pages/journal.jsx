cd679e72a7f0e45d1906e74593d5c8c3e2ab9eb5
Srusti Prusty <srustiprusty@gmail.com>
2023-04-26 17:16:16 +0530

    fonts fixed

--- src/pages/journal.jsx
+++ src/pages/journal.jsx
@@ -14,13 +14,13 @@ const Journal = () => {
                 <div className="textt">
                     <h1 className='journal-heading'> Let out your thoughts. 🧘🏻  </h1>
                     <h2 className='journal-text1'>
-                        Often releasing your thoughts out in a diary makes you feel much better. So note down or record all that you want to rant and the entry remains with you. We will predict how your day was .... 
+                        Often releasing your thoughts out in a diary makes you feel much better. So note down or record all that you want to rant and the entry remains with you. We will predict how your day was. 
                     </h2>
                     <h3 className='journal-text2'>
                         Sounds interesting right?
                     </h3>
                 <h4 className='journal-text3'>
-                    Then c'mmon, start recording 
+                    Then c'mmon, start recording !
                 </h4>
                 <img src="https://i.postimg.cc/13nVvSm5/607f7d902f5de-unscreen.gif"/>
                 </div>
